2567deb1e6b54aead1fc731ab20bcc0b34d30175
alex <klepa_82@bk.ru>
2023-08-15 18:18:05 +0300

    1

---
+++ models/Table.java
@@ -0,0 +1,28 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Table {
+    private static int counter;
+
+    private final int no;
+
+    {
+        no = ++counter;
+    }
+
+    private final ArrayList reservations = new ArrayList<>();
+
+    public ArrayList getReservations() {
+        return reservations;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Столик #%d", no);
+    }
+}
